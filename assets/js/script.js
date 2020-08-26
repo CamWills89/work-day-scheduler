386af5f4d8c33d6a80aeb11997114829ae690794
Cameron <cameron.wills89@outlook.com>
2020-08-26 14:38:27 -0500

    added bootstrap grid and matched css selectors to stylesheet

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Get current day and display it in the header
+
+
+//color code the blocks to according to time of day
+//past
+//present
+//future
+
+//link text area text to the save button to store that in local storage
+//so that the events persist
