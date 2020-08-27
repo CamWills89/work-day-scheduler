b3ac4c2ade72c0520d2c49ec59e74ab738634534
Cameron <cameron.wills89@outlook.com>
2020-08-27 14:37:47 -0500

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,63 @@
-tasks = {};
-
 //Get current day and display it in the header
 var today = moment().format("dddd, MMMM, Do");
 var currentDay = $("#currentDay").text(today);
-
-// var loadTasks = function () {
-//     tasks = JSON.parse(localStorage.getItem("tasks"));
-
-//     // if nothing in localStorage, create a new object to track all task status arrays
-//     if (!tasks) {
-//       tasks = {
-//         event: []
-//       };
-//     }
-
-//     // loop over object properties
-//     $.each(tasks, function (list, arr) {
-//       // then loop over sub-array
-//       arr.forEach(function (task) {
-//         createTask(task.text, task.date, list);
-//       });
-//     });
-//   };
-
-//   var saveTasks = function () {
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//   };
 
 //color code the blocks to according to time of day
 //past
 //present
 //future
 
-var getText = $("#time-9").on("click", "textarea", function () {
-  var text = $(this).text().trim();
-  console.log(text);
-});
+// var setEvent = function () {
+//   $(".saveBtn").click(function () {
+//     var text = $("#time-9 textarea").val();
+//     console.log(text);
 
-var setEvent = function () {
+//     var userEvent = {
+//       text: text,
+//     };
 
-  $(".saveBtn").click(function () {
-    var text = $("#time-9 textarea").val();
+//     localStorage.setItem("userEvent", JSON.stringify(userEvent));
+//   });
+// };
+
+// var loadEvent = function () {
+//   var userEvent = JSON.parse(localStorage.getItem("userEvent"));
+//   if (!userEvent) {
+//     userEvent = [];
+//   }
+//   if (userEvent) {
+//     $("#time-9 textarea").html(userEvent.text);
+//   } else {
+//     setEvent();
+//   }
+// };
+//this will let me select all the textareas
+var getText = function () {
+  $(".time-block").on("click", "textarea", function () {
+    var text = $(this).text().trim();
+    console.log(text);
+  });
+  saveText();
+};
+
+var saveText = function () {
+  $(".time-block").on("click", ".saveBtn", function () {
+    var text = $(".time-block textarea").val()
+    
     console.log(text);
 
-    var userEvent = {
-      text: text,
-    };
+    var status = $(this)
+    .closest(".time-block")
+    .attr("id");
+    console.log(status);
 
-    localStorage.setItem("userEvent", JSON.stringify(userEvent));
+    var index = $(this).closest(".time-block").index();
+    console.log(index);
   });
 };
 
-var loadEvent = function () {
-  var userEvent = JSON.parse(localStorage.getItem("userEvent"));
-  if (!userEvent) {
-    userEvent = [];
-  }
-  if(userEvent){
-  $("#time-9 textarea").html(userEvent.text);
-} else {
-    setEvent();
-}
-}
-//this will let me select all the textareas
-// $(".container").on("click", function() {
-//     var text = $(this).text().trim();
-//     console.log(text);
-
-//     var status = $(this)
-//     .closest(".time-block")
-//     console.log(status);
-
-//     // get the task's position in the list of other li elements
-//     var index = $(this)
-//     .closest("#time-14")
-//     .index();
-//     console.log(index);
-
-// })
 //link text area text to the save button to store that in local storage
 //so that the events persist
-loadEvent();
-setEvent();
+// loadEvent();
+// setEvent();
+getText();
