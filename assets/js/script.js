c0b9860a37f4574373794015517a88d4c987c87a
Cameron <cameron.wills89@outlook.com>
2020-08-28 10:51:53 -0500

    trying to load from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,80 +7,37 @@ var currentDay = $("#currentDay").text(today);
 //present
 //future
 
-// var setEvent = function () {
-//   $(".saveBtn").click(function () {
-//     var text = $("#time-9 textarea").val();
-//     console.log(text);
-
-//     var userEvent = {
-//       text: text,
-//     };
-
-//     localStorage.setItem("userEvent", JSON.stringify(userEvent));
-//   });
-// };
-
 var loadEvent = function () {
-  var userEvent = JSON.parse(localStorage.getItem("userEvent")) || [];
+  var userEvent = JSON.parse(localStorage.getItem("userEvent")) || {};
 console.log(userEvent);
-
-
-  for (let i = 0; i < userEvent.length; i++) {
-    // var listedEvent = $(".time-block").siblings("textarea");
-    $(".time-block textarea").text(userEvent[i].text);
-    // $(".time-block").siblings("textarea").append(userEvent[i])
+// debugger;
+for (let i = 0; i < userEvent.length; i++) {
+  $(".time-block#" + userEvent[i].id + "textarea").text(userEvent[i].text);
   }
-  // $.each(userEvent, function(arr){
-  //     arr.forEach(function (text) {
-  //         userEvent.text(text[i])
-  //     });
-  // })
-
-  //   if (userEvent) {
-  //     $("#time-9 textarea").html(userEvent.text);
-  //   } else {
-  //     setEvent();
-  //   }
-};
-//this will let me select all the textareas
-var getText = function () {
-  $(".time-block").on("click", "textarea", function () {
-    var text = $(this).text().trim();
-    console.log(text);
-  });
-  saveText();
 };
 
 var saveText = function () {
-  var userEvent = JSON.parse(localStorage.getItem("userEvent"));
+  var userEvent = JSON.parse(localStorage.getItem("userEvent")) || {};
 
-  if (!userEvent) {
-    userEvent = [];
-  }
   $(".time-block").on("click", ".saveBtn", function () {
+
     var text = $(this).siblings("textarea").val();
-
-    console.log(text);
-
-    // var currentEvent = text;
-    var currentEvent = {
-      text: text,
-    };
-
-    userEvent.push(currentEvent);
+    var id = $(this).closest(".time-block").attr("id");
+  
+    // var currentEvent = {
+    //   text: text,
+    //   id: id
+    // };
+    
+    userEvent[id] = text;
+    //previously had it as an array
+    // userEvent.push(currentEvent)
 
     localStorage.setItem("userEvent", JSON.stringify(userEvent));
-
-    var status = $(this).closest(".time-block").attr("id");
-    console.log(status);
-
-    var index = $(this).closest(".time-block").index();
-    console.log(index);
   });
 };
 
-//link text area text to the save button to store that in local storage
-//so that the events persist
 loadEvent();
-// setEvent();
-getText();
+saveText();
+
+
